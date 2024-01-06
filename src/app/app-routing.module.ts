2235cdf4d377ded3e4c13ebde67ba0e8ff8aa0de
black-adm <mattheuscontato17@gmail.com>
2024-01-06 20:46:10 -0300

    config modules and added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { WorkspaceComponent } from './components/inside/workspace/workspace.component';
+import { BoardComponent } from './components/inside/board/board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent,
+  },
+  {
+    path: "workspace",
+    component: WorkspaceComponent,
+  },
+  {
+    path: "workspace/:id",
+    component: BoardComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "/",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
