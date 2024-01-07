4d7ccb3c2da70f2c718f1b19be0ff0416d236e71
black-adm <mattheuscontato17@gmail.com>
2024-01-06 22:16:54 -0300

    feat: auth service and login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  email = 'mattheuscontato17@gmail.com'
+  linkSucces = false
 
+  constructor(
+    private auth: AuthService,
+    private spinner: NgxSpinnerService,
+    private router: Router
+    ) {}
+
+  ngOnInit(): void {}
+
+  async signIn() {
+    this.spinner.show()
+    const result = await this.auth.login(this.email)
+    console.log(result)
+
+    this.spinner.hide()
+    if(!result.error) this.linkSucces = true 
+    alert(result.error?.message)
+  }
 }
